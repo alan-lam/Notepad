fb9e0a9b28215db587209c1198175d482d244441
alan-lam <alanlam00@gmail.com>
2018-10-01 14:40:28 -0700

    added all notes activity

---
+++ app/src/main/java/com/example/alan/notepad/AllNotes.java
@@ -0,0 +1,13 @@
+package com.example.alan.notepad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllNotes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_notes);
+    }
+}
--- app/src/main/java/com/example/alan/notepad/MainActivity.java
+++ app/src/main/java/com/example/alan/notepad/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.alan.notepad;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -13,6 +15,7 @@ import java.io.OutputStreamWriter;
 public class MainActivity extends AppCompatActivity {
 
     EditText edittext;
+    Intent allNotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +35,13 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.documents, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        allNotes = new Intent(MainActivity.this, AllNotes.class);
+        startActivity(allNotes);
+        return false;
     }
 
     public void Save(String fileName) {
