5e9e2531460e80e79e70ff99386515c299d65166
alan-lam <alanlam00@gmail.com>
2018-10-01 14:23:52 -0700

    added show all notes menu

--- app/src/main/java/com/example/alan/notepad/MainActivity.java
+++ app/src/main/java/com/example/alan/notepad/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.alan.notepad;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edittext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.save);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Save("a.txt");
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.documents, menu);
+        return true;
+    }
+
+    public void Save(String fileName) {
+        edittext = findViewById(R.id.edittext);
+        try {
+            OutputStreamWriter out = new OutputStreamWriter(openFileOutput(fileName, 0));
+            out.write(edittext.getText().toString());
+            out.close();
+            Toast.makeText(this, "Saved", Toast.LENGTH_SHORT).show();
+        }
+        catch (Throwable t) {
+            Toast.makeText(this, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
     }
 }
