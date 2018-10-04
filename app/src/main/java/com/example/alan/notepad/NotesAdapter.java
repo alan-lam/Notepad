04741f14ac6f7121d27c8e42a63ecf9d9d6dd5bc
alan-lam <alanlam00@gmail.com>
2018-10-03 20:43:17 -0700

    removed delete from listview

--- app/src/main/java/com/example/alan/notepad/NotesAdapter.java
+++ app/src/main/java/com/example/alan/notepad/NotesAdapter.java
@@ -1,24 +1,18 @@
 package com.example.alan.notepad;
 
 import android.app.Activity;
-import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
-import java.io.File;
 import java.util.ArrayList;
 
 public class NotesAdapter extends ArrayAdapter<Note> {
 
-    ArrayList<Note> list;
-
     public NotesAdapter(Activity context, ArrayList<Note> notes) {
         super(context, 0, notes);
-        this.list = notes;
     }
 
     @Override
@@ -32,18 +26,6 @@ public class NotesAdapter extends ArrayAdapter<Note> {
 
         TextView title = listItemView.findViewById (R.id.title);
         title.setText (currentNote.getTitle());
-
-        ImageButton deleteButton = listItemView.findViewById(R.id.deleteButton);
-        deleteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String path = Environment.getExternalStorageDirectory().toString()+"/Notes/"+list.get(position).getTitle();
-                File file = new File(path);
-                file.delete();
-                list.remove(position);
-                notifyDataSetChanged();
-            }
-        });
 
         return listItemView;
     }
